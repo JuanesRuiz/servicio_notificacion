92c0d3adbcaf60d0cf2c98acbb0ef14bbfa9b1b0
JuanesRuiz <111442158+JuanesRuiz@users.noreply.github.com>
2025-07-31 17:47:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from flask import Flask, request, jsonify
 
-app = Flask (__name__)
+app = Flask(__name__)
 
-#Crear la ruta del endpoint
 @app.route('/notificar', methods=['POST'])
 def notificar():
-    data = request.json
-    nombre = data.get("nombre", "desconocido")
-    print (f" notificando a {nombre}...")
-    return jsonify({"Notificacion": f"Se notifico a {nombre} exitosamente."})
+    # Verifica si el Content-Type es application/json
+    if not request.is_json:
+        return jsonify({"error": "Content-Type debe ser application/json"}), 400
+    
+    try:
+        data = request.json
+        nombre = data.get("nombre", "desconocido")
+        print(f"Notificando a {nombre}...")
+        return jsonify({"Notificacion": f"Se notificó a {nombre} exitosamente."})
+    except Exception as e:
+        return jsonify({"error": f"Error procesando la solicitud: {str(e)}"}), 400
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 10000)
+    app.run(host='0.0.0.0', port=10000)
